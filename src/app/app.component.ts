54844d75ca5c806f96131ca10ff1202945088ad8
U0K2399 <andrea.midena@gft.com>
2022-11-03 16:10:16 +0100

    icon working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
 
 @Component({
   selector: 'ePocCM-root',
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-pippo
     <router-outlet></router-outlet>
-  `,
+    <ePocCM-dashboard></ePocCM-dashboard>`,
   styles: []
 })
 export class AppComponent {
   title = 'editorPOCCodeMirror';
+  constructor(matIconRegistry: MatIconRegistry) { matIconRegistry.registerFontClassAlias('fontawesome', 'fa'); }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,36 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { LayoutModule } from '@angular/cdk/layout';
+import { ToolbarComponent } from './components/dashboard/toolbar/toolbar.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    LayoutModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
